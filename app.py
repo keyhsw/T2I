ff7e229e281258630f63d50a1f984054eb9f74a0
yizhangliu <yizhangliu@users.noreply.huggingface.co>
2022-12-07 15:37:41 +0000

    Update app.py

--- app.py
+++ app.py
@@ -15,11 +15,12 @@ def getTextTrans(text, source='zh', target='en'):
 
 
 model_ids = {
+            "models/stabilityai/stable-diffusion-2-1":"sd-v2-1",
             "models/stabilityai/stable-diffusion-2":"sd-v2-0",
             "models/runwayml/stable-diffusion-v1-5":"sd-v1-5",
             "models/CompVis/stable-diffusion-v1-4":"sd-v1-4",
             "models/prompthero/openjourney":"openjourney",
-            "models/ShadoWxShinigamI/Midjourney-Rangoli":"midjourney",
+            # "models/ShadoWxShinigamI/Midjourney-Rangoli":"midjourney",
             "models/hakurei/waifu-diffusion":"waifu-diffusion",
             "models/Linaqruf/anything-v3.0":"anything-v3.0",
            }
